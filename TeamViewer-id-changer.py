4fdc4a932d1f0bd6453b2e8fd0ea79dbb61a2da7
JimLee1996 <59061463@qq.com>
2019-09-12 10:36:28 +0800

    update teamviewer crack tool

--- TeamViewer-id-changer.py
+++ TeamViewer-id-changer.py
@@ -1,77 +1,71 @@
-#!/usr/bin/python2.7
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-#coding:utf-8
-import sys
 import os
-import glob
 import platform
-import re
 import random
+import re
 import string
+import sys
 
-print(
-    '''
+print('''
 --------------------------------
-TeamViewer ID Changer for MAC OS
+TeamViewer 14 ID Changer for MAC OS
+Version: 0.2 2019
 --------------------------------
-'''
-)
+''')
 
-if platform.system() != 'Darwin':
-    print('This script can be run only on MAC OS.')
+if platform.system() != "Darwin":
+    print("This script can be run only on MAC OS.")
     sys.exit()
 
 if os.geteuid() != 0:
-    print('This script must be run form root.')
+    print("This script must be run form root.")
     sys.exit()
 
-if os.environ.has_key('SUDO_USER'):
-    USERNAME = os.environ['SUDO_USER']
-    if USERNAME == 'root':
-        print(
-            'Can not find user name. Run this script via sudo from regular user'
-        )
+if "SUDO_USER" in os.environ:
+    USERNAME = os.environ["SUDO_USER"]
+    if USERNAME == "root":
+        print("Can not find user name. Run this script via sudo from regular user")
         sys.exit()
 else:
-    print('Can not find user name. Run this script via sudo from regular user')
+    print("Can not find user name. Run this script via sudo from regular user")
     sys.exit()
 
-HOMEDIRLIB = '/Users/' + USERNAME + '/library/preferences/'
-GLOBALLIB = '/library/preferences/'
+HOMEDIRLIB = "/Users/" + USERNAME + "/library/preferences/"
+GLOBALLIB = "/library/preferences/"
 
 CONFIGS = []
 
-# Find config files
 
+# Find config files
 
 def listdir_fullpath(d):
     return [os.path.join(d, f) for f in os.listdir(d)]
 
 
 for file in listdir_fullpath(HOMEDIRLIB):
-    if 'teamviewer'.lower() in file.lower():
+    if 'teamviewer' in file.lower():
+        CONFIGS.append(file)
+
+for file in listdir_fullpath(GLOBALLIB):
+    if 'teamviewer' in file.lower():
         CONFIGS.append(file)
 
 if not CONFIGS:
-    print(
-        '''
+    print('''
 There is no TemViewer configs found.
 Maybe you have deleted it manualy or never run TeamViewer after installation.
 Nothing to delete.
-'''
-    )
-# Delete config files
+''')
 else:
+    # Delete config files
     print("Configs found:\n")
-    for file in CONFIGS:
-        print file
-
-    print(
-        '''
+    for file in CONFIGS: print(file)
+    print('''
 This files will be DELETED permanently.
 All TeamViewer settings will be lost
-'''
-    )
+''')
     raw_input("Press Enter to continue or CTR+C to abort...")
 
     for file in CONFIGS:
@@ -88,6 +82,7 @@ TMBINARYES = [
     '/Applications/TeamViewer.app/Contents/MacOS/TeamViewer',
     '/Applications/TeamViewer.app/Contents/MacOS/TeamViewer_Service',
     '/Applications/TeamViewer.app/Contents/Helpers/TeamViewer_Desktop',
+    '/Applications/TeamViewer.app/Contents/Helpers/TeamViewer_Assignment'
 ]
 
 for file in TMBINARYES:
@@ -95,49 +90,45 @@ for file in TMBINARYES:
         pass
     else:
         print("File not found: " + file)
-        print("Install TeamViewer correctly")
+        print ("Install TeamViewer correctly")
         sys.exit()
 
-# Patch files
 
+# Patch files
 
 def idpatch(fpath, platf, serial):
     file = open(fpath, 'r+b')
     binary = file.read()
     PlatformPattern = "IOPlatformExpert.{6}"
-    SerialPattern = "IOPlatformSerialNumber%s%s%sUUID"
+    SerialPattern = "IOPlatformSerialNumber%s%s%s"
 
     binary = re.sub(PlatformPattern, platf, binary)
-    binary = re.sub(
-        SerialPattern % (chr(0), "[0-9a-zA-Z]{8,8}", chr(0)),
-        SerialPattern % (chr(0), serial, chr(0)), binary
-    )
+    binary = re.sub(SerialPattern % (chr(0), "[0-9a-zA-Z]{8,8}", chr(0)), SerialPattern % (chr(0), serial, chr(0)), binary)
 
     file = open(fpath, 'wb').write(binary)
     return True
 
 
-def random_generator(size=8, chars=string.ascii_uppercase + string.digits):
+def random_generator(size=8, chars=string.ascii_uppercase + string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 
-RANDOMSERIAL = random_generator()
+RANDOMSERIAL = random_generator(8)
 RANDOMPLATFORM = "IOPlatformExpert" + random_generator(6)
 
 for file in TMBINARYES:
     try:
         idpatch(file, RANDOMPLATFORM, RANDOMSERIAL)
     except:
-        print "Error: can not patch file " + file
-        print "Wrong version?"
+        print("Error: can not patch file " + file)
         sys.exit()
 
-print "PlatformDevice: " + RANDOMPLATFORM
-print "PlatformSerial: " + RANDOMSERIAL
+print("PlatformDevice: " + RANDOMPLATFORM)
+print("PlatformSerial: " + RANDOMSERIAL)
 
-print(
-    '''
+os.system("sudo codesign -f -s - /Applications/TeamViewer.app/")
+
+print('''
 ID changed sucessfully.
 !!! Restart computer before using TeamViewer !!!!
-'''
-)
+''')
